93a0d500633f939710b0d47d38e62abff4bfc269
Aidan Wolter <aidanwolter@letu.edu>
2013-06-30 02:55:08 -0500

    added add functionality

--- pang.js
+++ pang.js
@@ -1,49 +1,74 @@
 var Pang = (function(){
-        var pang = {};
-        pang.connections = [];
-        pang.init = function(data) {
-            Parse.initialize(data.appKey,data.jsKey);
-        }
+    var pang = {};
+    pang.connections = [];
+    pang.init = function(data) {
+        Parse.initialize(data.appKey,data.jsKey);
+    }
 
-        pang.addConnection = function(parseClassName, angularArray) {
-            var connection = (function() {
-                conn = {};
-                conn.parseClassName = parseClassName;
-                conn.angularArray = angularArray;
-                conn.parseObject = Parse.Object.extend(parseClassName);
-                conn.query = new Parse.Query(conn.parseObject);
-                conn.fetch = function() {
-                    conn.query.find({
-                        success: function(results) {
-                            angularObjects = [];
-                            for(var index in results) {
-                                var result = results[index];
-                                angularObject = {};
-                                for(var key in result.attributes) {
-                                    angularObject[key.toString()] = result.get(key.toString());
-                                }
-                                angularObject.parseObject = result;
-                                angularObjects.push(angularObject);
+    pang.addConnection = function(parseClassName, angularArray) {
+        var connection = (function() {
+            conn = {};
+            conn.parseClassName = parseClassName;
+            conn.angularArray = angularArray;
+            conn.parseObject = Parse.Object.extend(parseClassName);
+            conn.query = new Parse.Query(conn.parseObject);
+
+            //functions which each connection implements
+            conn.fetch = function() {
+                conn.query.find({
+                    success: function(results) {
+                        angularObjects = [];
+                        for(var index in results) {
+                            var result = results[index];
+                            angularObject = {};
+                            for(var key in result.attributes) {
+                                angularObject[key.toString()] = result.get(key.toString());
                             }
-                            $scope.$apply(function() {
-                                $scope[conn.angularArray] = angularObjects;
-                            });
-                        },
-                        error: function(error) {
-                            console.log('Error: '+error);
+                            angularObject.parseObject = result;
+                            angularObjects.push(angularObject);
                         }
-                    });
-                }
-                return conn;
-            })();
-            this.connections = [connection];
-        }
+                        $scope.$apply(function() {
+                            $scope[conn.angularArray] = angularObjects;
+                        });
+                    },
+                    error: function(error) {
+                        console.log('Error: '+error);
+                    }
+                });
+            }
 
-        pang.fetch = function() {
-            for(var index in this.connections) {
-                this.connections[index].fetch();
+            conn.add = function(data) {
+                var obj = new conn.parseObject();
+                obj.save(data,{
+                    error: function(error) {
+                        console.log('Error, could not save: '+error)
+                    }
+                });
+                var angularObject = data;
+                data.parseObject = obj;
+                $scope.jobs.push(angularObject);
+            }
+
+            return conn;
+        })();
+        this.connections = [connection];
+    }
+
+    pang.fetch = function() {
+        for(var index in this.connections) {
+            this.connections[index].fetch();
+        }
+    }
+
+    pang.add = function(parseClassName, data) {
+        for(var index in this.connections) {
+            connection = this.connections[index];
+            if(connection.parseClassName == parseClassName) {
+                connection.add(data);
+                break;
             }
         }
-        return pang;
-    })();
+    }
 
+    return pang;
+})();
