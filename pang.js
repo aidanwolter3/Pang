1ba3788fd396a680e285eca98248c707bd04ee62
Aidan Wolter <aidanwolter@letu.edu>
2013-08-17 23:55:21 -0500

    made functions work without promises

--- pang.js
+++ pang.js
@@ -16,85 +16,129 @@ PangObject.prototype.initialize = function() {
     pangObject.data = [];
     pangObject.parseObjects = [];
 
-    var promise = {};
+    
     var parseObject = Parse.Object.extend(this.className);
     var query = new Parse.Query(parseObject);
 
-    promise.then = function(successFtn, errorFtn) {
-        query.find({
-            success: function(objects) {
-                for(var index in objects) {
-                    object = objects[index];
-                    dataObject = $.extend({parseObjectId: object.id},object.attributes);
-                    pangObject.parseObjects.push(object);
-                    pangObject.data.push(dataObject);
-                }
+    //set up the promise
+    var promise = {};
+    var successFtn;
+    var errorFtn;
+    promise.then = function(s, e) {
+        successFtn = s;
+        errorFtn = e;
+    }
+
+    //get all the objects from parse
+    query.find({
+        success: function(objects) {
+            for(var index in objects) {
+                object = objects[index];
+                dataObject = $.extend({parseObjectId: object.id}, object.attributes);
+                pangObject.parseObjects.push(object);
+                pangObject.data.push(dataObject);
+            }
+            if(successFtn) {
                 successFtn();
-            },
-            error: function(error) {
-                console.log('Pang encountered an error while fetching data from Parse!');
+            }
+        },
+        error: function(error) {
+            console.log('Pang encountered an error while fetching data from Parse!');
+            if(errorFtn) {
                 errorFtn();
             }
-        });
-    }
+        }
+    });
+
     return promise;
 }
 
 PangObject.prototype.add = function(newData) {
     var pangObject = this;
-
-    var promise = {};
     var parseObject = Parse.Object.extend(this.className);
     var newParseObject = new parseObject;
 
-    promise.then = function(successFtn, errorFtn) {
-        newParseObject.save(newData, {
-            success: function(object) {
-                pangObject.data.concat($.extend({parseObjectId: object.id},object.attributes));
-                pangObject.parseObjects.push(object);
-                successFtn(object.id);
-            },
-            error: function(error) {
-                errorFtn(null);
-            }
-        });
+    //set up the promise
+    var promise = {};
+    var successFtn;
+    var errorFtn;
+    promise.then = function(s, e) {
+        successFtn = s;
+        errorFtn = e;
     }
+
+    //save the new data
+    newParseObject.save(newData, {
+        success: function(object) {
+            pangObject.data.push($.extend({parseObjectId: object.id}, object.attributes));
+            pangObject.parseObjects.push(object);
+            if(successFtn) { 
+                successFtn(object.id);
+            }
+        },
+        error: function(error) {
+            if(errorFtn) {
+                errorFtn();
+            }
+        }
+    });
+
     return promise;
 }
 
 PangObject.prototype.delete = function(parseObjectId) {
     var pangObject = this;
 
-    var promise = {};
+    //find the parseObject
     var parseObject;
+    var parseObjectIndex;
     for(var index in pangObject.parseObjects) {
         var object = pangObject.parseObjects[index];
         if(object.id == parseObjectId) {
+            parseObjectIndex = index;
             parseObject = object;
             break;
         }
     }
 
-    promise.then = function(successFtn, errorFtn) {
-        if(!parseObject) {
+    //set up the promise
+    var promise = {};
+    var successFtn;
+    var errorFtn;
+    promise.then = function(s, e) {
+        successFtn = s;
+        errorFtn = e;
+    }
+
+    //if the parseObject could not be found
+    if(!parseObject) {
+        if(errorFtn) {
             errorFtn();
-            return;
+        }
+        return;
+    }
+
+    //destroy the data
+    parseObject.destroy({
+        success: function() {
+        for(var index in pangObject.data) {
+            var object = pangObject.data[index];
+            if(object.parseObjectId == parseObjectId) {
+                pangObject.data.splice(index, 1);
+            }
+        }
+        pangObject.parseObjects.splice(parseObjectIndex, 1);
+        if(successFtn) {
+            successFtn();
         }
 
-        parseObject.destroy({
-            success: function() {
-            for(var index in pangObject.data) {
-                var object = pangObject.data[index];
-                if(object.parseObjectId == parseObjectId) {
-                    delete object;
-                }
-            }
-            successFtn();
-            }, 
-            error: function() {
+        }, 
+        error: function() {
+            if(errorFtn) {
                 errorFtn();
             }
-        })
-    }
+        }
+    })
+
     return promise;
 }
