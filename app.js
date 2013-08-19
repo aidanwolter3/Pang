7a452493f4face6d750e9e73b0f18111fcbf75ef
Aidan Wolter <aidanwolter@letu.edu>
2013-08-18 23:50:16 -0500

    small fix in delete function error log

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function PangTestCtrl($scope, PangObject) {
 			console.log('Deleted object');
 			$scope.$apply();
 		}, function() {
-			console.lof('Error deleting object!');
+			console.log('Error deleting object!');
 		});
 	}
 
